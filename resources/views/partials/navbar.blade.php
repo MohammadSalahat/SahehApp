abf5631f81a8b4eca85c7b0925ba5db8d8c87192
“Mohammad <mohammadsalahat691@gmail.coom>
2025-11-10 17:47:52 +0300

    refactor: Update navbar logo section to use anchor tag for better navigation

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,10 +2,10 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
             <!-- Logo -->
-            <div class="flex items-center">
+            <a class="flex items-center cursor-pointer " href="{{ route('home') }}" wire:navigate>
                 <x-app-logo-icon class="h-10 object-contain" />
                 <span class="ml-2 text-xl font-semibold text-primary">{{ __('navigation.name') }}</span>
-            </div>
+            </a>
 
             <!-- Desktop Navigation -->
             <div class="hidden md:flex items-start  gap-4">
